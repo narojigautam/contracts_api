0df059e28b383f862226d73a3d9cbac7e64d907d
Gautam Naroji <gautam@appyantra.com>
2016-08-02 19:08:40 +0530

    enabled cross origins in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Raises error for missing translations
+  # config.action_view.raise_on_missing_translations = true
+  config.middleware.insert_before 0, "Rack::Cors" do
+    allow do
+      # In development, we don't care about the origin.
+      origins '*'
+      # Reminder: On the following line, the 'methods' refer to the 'Access-
+      # Control-Request-Method', not the normal Request Method.
+      resource '*', :headers => :any, :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'], :methods => [:get, :post, :options, :delete, :put, :patch], credentials: true
+    end
+  end
 end
